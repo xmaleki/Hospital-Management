2d8b920a3bc287d3f0a935cbd2f6bf1981f6a1d9
abolfazl <maleki.direct@gmail.com>
2026-09-19 20:13:14 +0330

    Add emergency.h

---
+++ emergency.h
@@ -0,0 +1,16 @@
+#ifndef EMERGENCY_H
+#define EMERGENCY_H
+#include<string>
+
+using namespace std;
+
+void emergencySystem(string);
+void calculatebloodpressure(string);
+void checkdruginteraction(string);
+void determinebloodgroup(string);
+void calculateGCS(string);
+void bankinventory(string);
+void increasebankinventory(string);
+void checkbloodbankfile();
+
+#endif
