f45a52c30c803c1df3af558e1190d58c6bf465d8
abolfazl <maleki.direct@gmail.com>
2026-09-19 20:12:26 +0330

    Add admin.h

---
+++ admin.h
@@ -0,0 +1,11 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+#include <string>
+
+using namespace std;
+
+void adminSystem();
+void showList(string filename, string title);
+void addToList(string filename);
+
+#endif
