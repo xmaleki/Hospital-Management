aec8093830b32bfb6433f52d7275109d6815b327
abolfazl <maleki.direct@gmail.com>
2026-09-19 20:16:01 +0330

    Add psychic.h

---
+++ psychic.h
@@ -0,0 +1,12 @@
+#ifndef PSYCHIC_H
+#define PSYCHIC_H
+#include<string>
+
+using namespace std;
+
+void psychicSystem(string);
+void anxiety(string);
+void depression(string);
+void stress(string);
+
+#endif
