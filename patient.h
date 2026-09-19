4fb9d94a7f485ad78a78c3d97f1f1b102d1c41a5
abolfazl <maleki.direct@gmail.com>
2026-09-19 20:14:07 +0330

    Add patient.h

---
+++ patient.h
@@ -0,0 +1,16 @@
+#ifndef PATIENT_H
+#define PATIENT_H
+
+#include<string>
+
+using namespace std;
+void patientSystem(string);
+bool patientexists(string);
+void createpatient(string);
+string getvalue(string, string);
+string choosedepartment(string);
+
+
+
+
+#endif
